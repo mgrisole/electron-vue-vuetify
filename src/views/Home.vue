668498365c7d0a74b35235728defeb37bae2ff53
mgrisole <grisole.maxime@gmail.com>
2018-08-24 11:46:11 +0200

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,24 @@
 <template lang='pug'>
-    v-container(fill-height)
-      v-layout(column align-center)
-        v-layout(align-center)
-          | content
-        v-layout(align-center)
-          | content
+  v-container(fill-height)
+    v-layout(column align-center)
+      ModuleTimer
+      ModuleTimer
 </template>
+
+<script>
+import ModuleTimer from '@/components/modules/ModuleTimer'
+
+export default {
+  name: 'Home',
+  components: {
+    ModuleTimer
+  },
+  data () {
+    return {}
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
